4ed5ac3fa5d95c85cc64c96337bde48740f63242
Akn <sibata486@gmail.com>
2023-09-07 01:19:05 +0900

    add test / テストプロジェクトを追加

---
+++ ImageChecker_2Tests/Models/ImageFileTest.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace ImageChecker_2Tests.Models
+{
+    public class ImageFileTest
+    {
+        [Test]
+        public void METHOD()
+        {
+        }
+    }
+}
