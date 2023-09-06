f2252961e28289da78658b9341da0f8666f14e2b
Akn <sibata486@gmail.com>
2023-09-07 00:35:48 +0900

    init

---
+++ ImageChecker_2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Mvvm;
+
+namespace ImageChecker_2.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Image checker ver 2";
+
+        public MainWindowViewModel()
+        {
+        }
+        
+        public string Title { get => title; set => SetProperty(ref title, value); }
+    }
+}
---
+++ ImageChecker_2/Views/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ImageChecker_2.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
