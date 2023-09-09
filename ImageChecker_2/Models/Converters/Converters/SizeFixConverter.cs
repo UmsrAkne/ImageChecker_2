81d9dd609ece3282a037178c4bf6e3fd8c6d2037
Akn <sibata486@gmail.com>
2023-09-09 15:05:09 +0900

    add file / リストボックスアイテムの幅を調節するコンバーターを追加

---
+++ ImageChecker_2/Models/Converters/Converters/SizeFixConverter.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ImageChecker_2.Models.Converters.Converters
+{
+    public class SizeFixConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null ? (double)value - 20 : 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
