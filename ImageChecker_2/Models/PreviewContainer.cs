686f4bcd658fa18f6db0a2cbca02ae1b7f9aeb50
Akn <sibata486@gmail.com>
2023-09-10 23:56:51 +0900

    add file / 画面上にプレビューする ImageFile をまとめるクラスを追加

---
+++ ImageChecker_2/Models/PreviewContainer.cs
@@ -0,0 +1,27 @@
+namespace ImageChecker_2.Models
+{
+    public class PreviewContainer
+    {
+        public double X { get; set; }
+        
+        public double Y { get; set; }
+        
+        public double Scale { get; set; }
+        
+        public ImageFile ImageFileA { get; set; }
+        
+        public ImageFile ImageFileB { get; set; }
+        
+        public ImageFile ImageFileC { get; set; }
+        
+        public ImageFile ImageFileD { get; set; }
+        
+        public void SetImageFiles(ImageFile imageA, ImageFile imageB, ImageFile imageC, ImageFile imageD)
+        {
+            ImageFileA = imageA;
+            ImageFileB = imageB;
+            ImageFileC = imageC;
+            ImageFileD = imageD;
+        }
+    }
+}
