2cc9fb10a671f16360701fa5e50e364328fe10aa
Akn <sibata486@gmail.com>
2023-09-12 20:12:54 +0900

    add test / TagGenerator.GetTag のテストを追加

--- ImageChecker_2/Models/TagGenerator.cs
+++ ImageChecker_2/Models/TagGenerator.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 
@@ -7,23 +8,35 @@ namespace ImageChecker_2.Models
 {
     public static class TagGenerator
     {
-        public static string GetTag(string baseText, List<ImageFile> imageFiles)
+        /// <summary>
+        /// baseText と imageFiles の情報を使ってテキストを生成します。
+        /// </summary>
+        /// <param name="baseText">
+        /// この引数に含まれる文字列から　"$a", "$b", "$c", "$d" の４つの文字列が置き換えされます。
+        /// </param>
+        /// <param name="imageFiles">
+        /// 第一引数の baseText の特定の文字列が、 imageFiles の中のファイル名へと置き換えられます。
+        /// この引数に入力するリストのサイズは 4 にしてください。サイズがそれ以外の場合は例外がスローされます。
+        /// 尚、要素が null の場合は、baseText の該当文字列は空文字に置き換えられます。
+        /// </param>
+        /// <returns>baseText に置き換え処理を加えた文字列を返します。</returns>
+        /// <exception cref="ArgumentException">imageFiles が null であるか、リストのサイズが 4 以外の場合にスローされます。</exception>
+        public static string GetTag(string baseText, [NotNull]List<ImageFile> imageFiles)
         {
-            if (imageFiles.Count < 3 || imageFiles.Any(i => i.FileInfo == null))
+            if (imageFiles.Count != 4)
             {
                 throw new ArgumentException("不正な引数");
             }
-            
-            var a = Path.GetFileNameWithoutExtension(imageFiles[0].FileInfo.FullName);
-            var b = Path.GetFileNameWithoutExtension(imageFiles[1].FileInfo.FullName);
-            var c = Path.GetFileNameWithoutExtension(imageFiles[2].FileInfo.FullName);
-            var d = Path.GetFileNameWithoutExtension(imageFiles[3].FileInfo.FullName);
+
+            var reps = imageFiles.Select(
+                f => f == null ? string.Empty : Path.GetFileNameWithoutExtension(f.FileInfo.FullName))
+                .ToList();
 
             return baseText
-                .Replace("$a", a)
-                .Replace("$b", b)
-                .Replace("$c", c)
-                .Replace("$d", d);
+                .Replace("$a", reps[0])
+                .Replace("$b", reps[1])
+                .Replace("$c", reps[2])
+                .Replace("$d", reps[3]);
         }
     }
 }
