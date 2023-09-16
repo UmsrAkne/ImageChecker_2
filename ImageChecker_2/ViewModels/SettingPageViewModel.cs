d15f6bc5e4c5984a43efb883cca6d8b52fba87d4
Akn <sibata486@gmail.com>
2023-09-16 23:11:58 +0900

    add / 設定ページの機能を実装

--- ImageChecker_2/ViewModels/SettingPageViewModel.cs
+++ ImageChecker_2/ViewModels/SettingPageViewModel.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using ImageChecker_2.Models;
 using Prism.Commands;
 using Prism.Services.Dialogs;
 
@@ -6,23 +8,29 @@ namespace ImageChecker_2.ViewModels
 {
     public class SettingPageViewModel : IDialogAware
     {
+        private readonly string settingFileName = "setting.xml";
+        
         public event Action<IDialogResult> RequestClose;
 
         public string Title => string.Empty;
 
+        public Setting Setting { get; set; }
+        
         public DelegateCommand CloseCommand => new DelegateCommand(() =>
         {
             RequestClose?.Invoke(new DialogResult());
         });
-
+        
         public bool CanCloseDialog() => true;
 
         public void OnDialogClosed()
         {
+            Setting.Write(settingFileName, Setting);
         }
 
         public void OnDialogOpened(IDialogParameters parameters)
         {
+            Setting = File.Exists(settingFileName) ? Setting.Read(settingFileName) : new Setting();
         }
     }
 }
