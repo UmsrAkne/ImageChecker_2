9fcfb13f209cfc56e29e3d32ab3ca40e0363d703
Akn <sibata486@gmail.com>
2023-09-17 19:32:02 +0900

    add file / 拡大率の高低を表す enum を追加

---
+++ ImageChecker_2/Models/ZoomScale.cs
@@ -0,0 +1,11 @@
+#pragma warning disable SA1602
+namespace ImageChecker_2.Models
+{
+    public enum ZoomScale
+    {
+        Middle,
+        High,
+        Low,
+    }
+}
+#pragma warning restore SA1602
